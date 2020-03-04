493071c10c2a0c668c6c5d868eb499e0c09ae07b
zhousaito <zhousaito@163.com>
2020-03-04 16:46:05 +0800

    测试代码提交

--- app/src/main/java/com/sdxxtop/media/MainActivity.kt
+++ app/src/main/java/com/sdxxtop/media/MainActivity.kt
@@ -2,13 +2,14 @@ package com.sdxxtop.media
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Toast.makeText(this, "xxx", Toast.LENGTH_LONG).show()
 
     }
 }
